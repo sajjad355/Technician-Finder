1bd1ebb3a66e7501edf3faa3a5f4e2fbb4d5baeb
sajjad <sajjadurrahman3434@gmail.com>
2022-11-06 22:39:30 +0600

    Feat: Database Structure

--- dbControl/db.php
+++ dbControl/db.php
@@ -34,6 +34,127 @@ if ($conn->query($result) === TRUE) {
 }
  }
 
+function show ($conn)
+{
+    $q=$conn->query ("select * from user ");
+    return $q;
+
+}
+function deleteuser($conn,$idn)
+
+{
+    $q= $conn->query("DELETE FROM `user` WHERE `user`.`id` = '$idn'");
+   return $q;
+
+}
+function tTechnicianupdate($conn,$fullname, $username, $mobile,$gender, $password, $email,$birth,$experience,$catagory,$address,$file)
+ {
+$result =  "INSERT INTO `tTechnician` (`id`, `Fullname`, `username`, `mobile`, `gender`, `password`, `email`, `birth`, `experience`, `catagory`, `address`, `file`) VALUES (NULL, '$fullname', '$username', '$mobile', '$gender', '$password', '$email', '$birth', '$experience', '$catagory', '$address', '$file');";
+if ($conn->query($result) === TRUE) {
+    session_start();
+    $_SESSION["rahman"] = "rahman";
+    header('location:../HomePage.php');
+   
+
+   
+} else {
+   
+    echo "Error: " . $result . "<br>" . $conn->error;
+}
+ }
+function Technicianupdate($conn,$fullname, $username, $mobile,$gender, $password, $email,$birth,$experience,$catagory,$address,$file)
+ {
+$result =  "INSERT INTO `technician` (`id`, `Fullname`, `username`, `mobile`, `gender`, `password`, `email`, `birth`, `experience`, `category`, `address`, `file`) VALUES (NULL, '$fullname', '$username', '$mobile', '$gender', '$password', '$email', '$birth', '$experience', '$catagory', '$address', '$file');";
+if ($conn->query($result) === TRUE) {
+      
+      
+    ?>
+     <script> 
+     alert('Successful,you approved the technician');     
+      </script>
+      
+    <?php
+} else {
+   
+    echo "Error: " . $result . "<br>" . $conn->error;
+}
+
+ }
+ function deletetTechnician($conn,$idn)
+
+{
+    $q= $conn->query("DELETE FROM `tTechnician` WHERE tTechnician.`id` = '$idn'");
+   return $q;
+
+}
+function messag($conn,$uname,$tname,$tmes)
+ {
+$result=$conn->query("INSERT INTO `message` (`id`, `From1`, `To1`, `message`) VALUES (NULL, `$uname`, `$$tname`,`$tmes`)");
+if ($conn->query($result) === TRUE) {
+      
+      
+    ?>
+     <script> 
+     alert('Successful,Message is send');     
+      </script>
+    <?php
+} 
+else {
+   
+    echo "Error: " . $result . "<br>" . $conn->error;
+}
+
+
+ }
+
+
+
+ function showuser($conn,$uname,$pass)
+
+ {
+     $q= $conn->query("SELECT * FROM `user` WHERE (username='$uname' AND password='$pass')");
+    return $q;
+ }
+
+ function showtechnician($conn,$uname,$pass)
+
+ {
+     $q= $conn->query("SELECT * FROM `technician` WHERE (username='$uname' AND password='$pass')");
+    return $q;
+ }
+
+
+ function getuserbyid($conn,$id)
+
+ {
+     $q= $conn->query("SELECT * FROM `user` WHERE  id='$id' ");
+    return $q;
+ }
+
+
+
+ function userupdateinsert($conn,$fullname, $username, $mobile, $password, $email,$birth , $address,$id)
+ {
+
+$result="UPDATE `user` SET `name` = '$fullname', `username` = '$username', `mobile` = '$mobile', `password` = '$password', `email` = '$email', `birth-date` = '$birth', `address` = '$address' WHERE `user`.`id` = '$id';";
+if ($conn->query($result) === TRUE) {
+
+header('location:../userShowProfile.php');
+} else {
+   
+    echo "Error: " . $result . "<br>" . $conn->error;
+}
+ }
+
+
+
+ function gettechbyid($conn,$id)
+
+ {
+     $q= $conn->query("SELECT * FROM `technician` WHERE  id='$id' ");
+    return $q;
+ }
+
 
  function CloseCon($conn)
  {
